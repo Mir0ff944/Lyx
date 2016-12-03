4d7f5055895944a0e0fad91517ee9d7365c1e457
Miroslav <housera94@gmail.com>
2016-12-03 18:49:07 +0000

    frameworks and deatil veiews

---
+++ frameworks/Events/Events/Eventim.swift
@@ -0,0 +1,9 @@
+//
+//  Eventim.swift
+//  Events
+//
+//  Created by Miroslav Ivanov on 03/12/2016.
+//  Copyright © 2016 Miroslav Ivanov. All rights reserved.
+//
+
+import Foundation
---
+++ frameworks/Events/Events/Events.h
@@ -0,0 +1,19 @@
+//
+//  Events.h
+//  Events
+//
+//  Created by Miroslav Ivanov on 03/12/2016.
+//  Copyright © 2016 Miroslav Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Events.
+FOUNDATION_EXPORT double EventsVersionNumber;
+
+//! Project version string for Events.
+FOUNDATION_EXPORT const unsigned char EventsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Events/PublicHeader.h>
+
+
