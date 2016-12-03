d34d83647beff4b6ecb8aa01cbf11b08729720b8
Miroslav <housera94@gmail.com>
2016-12-03 15:54:23 +0000

    application icons

---
+++ Lyx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lyx
+//
+//  Created by Miroslav Ivanov on 01/12/2016.
+//  Copyright © 2016 Miroslav Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
